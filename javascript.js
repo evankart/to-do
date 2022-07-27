85598eeadbd02fcb886bf6f3afede35a54c81fc5
Evan Kartheiser <ekarthei@gmail.com>
2022-07-27 15:45:52 -0700

    adding Xs next to list items to remove

--- javascript.js
+++ javascript.js
@@ -10,7 +10,7 @@ function myFunction() {
   document.getElementById("new-item").value = ""; // returns the input value to "" so a new item can be added
 }
 
-// REMOVE ITEMS FROM LIST //
+// REMOVE FIRST ITEM FROM LIST //
 const removeItem = document.getElementById("remove-item");
 removeItem.addEventListener("click", removeFunction); // when the button is clicked, removeFunction runs
 
@@ -20,7 +20,29 @@ function removeFunction() {
   containerElement.removeChild(removeItem);
 }
 
-// TO DO: Add an 'x' next to each item on the listf that can remove that specific item.
+// TO DO: Add an 'x' next to each item on the list
+let myToDoList = document.getElementsByTagName("li"); // returns an array of all the items in the list
+let i;
+for (let i = 0; i < myToDoList.length; i++) {
+  // loops over all the items in the list
+  let span = document.createElement("span"); // creates a new span element
+  let txt = document.createTextNode("  X"); // creates a new text node with the 'x' character
+  span.className = "close"; // gives the x a class of 'close'
+  span.appendChild(txt); // makes '  X' the text child of the span element
+  myToDoList[i].appendChild(span); // appends the new span element to the relevant item in the list
+}
+
+// TO DO: When you click on the X the item next to it is removed
+let close = document.getElementsByClassName("close"); // gets all elements with the class 'close' (all the 'x' spans)
+let j;
+for (j = 0; j < close.length; j++) {
+  // loops through all the 'close' elements
+  close[i].onclick = function () {
+    // when you click on the x, run this function
+    let div = close[i].parentElement;
+    div.style.display = "none";
+  };
+}
 
 // TO DO: Drag to reorder list items.
 
