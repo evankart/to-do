7d16d3c1ac57a1a7963eacf6efbe95a2293d976a
Evan Kartheiser <ekarthei@gmail.com>
2022-08-02 21:44:42 -0700

    added functional close buttons to existing items. TO DO: extend functionality to new items as they're added

--- javascript.js
+++ javascript.js
@@ -8,6 +8,13 @@ function myFunction() {
   node.innerHTML = "<li>" + textnode + "</li>"; // places the input field value into <li> tags and places it in the newly created node
   document.getElementById("to-do-list").appendChild(node); // appends the new node to the "to-do-list" element (adds an item to the unordered list)
   document.getElementById("new-item").value = ""; // returns the input value to "" so a new item can be added
+
+  // TO DO: Add a close button to each element added to the list
+  let span = document.createElement("span"); // creates a new span element
+  let txt = document.createTextNode("  X"); // creates a new text node with the 'x' character
+  span.className = "close"; // gives the x a class of 'close'
+  span.appendChild(txt); // makes '  X' the text child of the span element
+  document.getElementById("to-do-list").appendChild(span); // appends the new span element to the relevant item in the list
 }
 
 // REMOVE FIRST ITEM FROM LIST //
@@ -20,7 +27,7 @@ function removeFunction() {
   containerElement.removeChild(removeItem);
 }
 
-// TO DO: Add an 'x' next to each item on the list
+// Add an 'x' next to each item on the list
 let myToDoList = document.getElementsByTagName("li"); // returns an array of all the items in the list
 let i;
 for (let i = 0; i < myToDoList.length; i++) {
@@ -32,18 +39,9 @@ for (let i = 0; i < myToDoList.length; i++) {
   myToDoList[i].appendChild(span); // appends the new span element to the relevant item in the list
 }
 
-// TO DO: When you click on the X the item next to it is removed
-let close = document.getElementsByClassName("close"); // gets all elements with the class 'close' (all the 'x' spans)
-let j;
-for (j = 0; j < close.length; j++) {
-  // loops through all the 'close' elements
-  close[i].onclick = function () {
-    // when you click on the x, run this function
-    let div = close[i].parentElement;
-    div.style.display = "none";
+// When you click on the X the item next to it is removed
+let spanTag = document.getElementsByClassName("close");
+for (i = 0; i < spanTag.length; i++)
+  spanTag[i].onclick = function () {
+    this.parentNode.remove(this);
   };
-}
-
-// TO DO: Drag to reorder list items.
-
-// TO DO: Auto put focus on the text field, allow to submit by just hiting enter and keep focus in the text field so you can keep adding items.
