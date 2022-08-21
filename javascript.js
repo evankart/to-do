58e9f532e6ba780039f6b02842d9c34ad6f18d4b
Evan Kartheiser <ekarthei@gmail.com>
2022-08-20 23:23:06 -0700

    added input validation to confirm field isn't empty

--- javascript.js
+++ javascript.js
@@ -3,7 +3,6 @@ let listArray = ["apples", "bananas", "hot dogs"];
 const listHTML = document.getElementById("to-do-list");
 
 for (let i = 0; i < listArray.length; i++) {
-  console.log("<li>" + listArray[i] + "</li>");
   let li = document.createElement("li");
   li.appendChild(document.createTextNode(listArray[i]));
 
@@ -17,6 +16,7 @@ for (let i = 0; i < listArray.length; i++) {
 }
 
 //------------------------------------------------------------
+
 document.querySelector("input").addEventListener("keydown", function (event) {
   if (event.keyCode === 13) {
     addToList(); // Adds the item to the list when the enter key is pressed
@@ -27,30 +27,35 @@ document.querySelector("input").addEventListener("keydown", function (event) {
 const addBtn = document.getElementById("btn");
 addBtn.addEventListener("click", addToList); // when the button is clicked, addToList function runs
 function addToList() {
-  console.log(document.getElementById("new-item").value);
   newItem = document.getElementById("new-item").value;
-  listArray.push(newItem);
-  console.log(listArray);
+  if (newItem != "") {
+    listArray.push(newItem);
 
-  //TO DO: after the new value is added to the array, the list is refreshed
-  let li = document.createElement("li");
-  li.appendChild(document.createTextNode(newItem));
-  let span = document.createElement("span"); // creates a new span element
-  let txt = document.createTextNode("X"); // creates a new text node with the 'x' character
-  span.className = "close"; // gives the x a class of 'close'
-  span.appendChild(txt); // makes '  X' the text child of the span element
-  li.appendChild(span); // appends the new span element to the relevant item in the list;
-  listHTML.appendChild(li);
+    //TO DO: after the new value is added to the array, the list is refreshed
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(newItem));
+    let span = document.createElement("span"); // creates a new span element
+    let txt = document.createTextNode("X"); // creates a new text node with the 'x' character
+    span.className = "close"; // gives the x a class of 'close'
+    span.appendChild(txt); // makes '  X' the text child of the span element
+    li.appendChild(span); // appends the new span element to the relevant item in the list;
+    listHTML.appendChild(li);
 
-  document.getElementById("new-item").value = "";
+    document.getElementById("new-item").value = "";
 
-  //TO DO: Add event handlers that remove the relevant list item when the delete button is pressed.
-  // When you click on the X the item next to it is removed
-  let spanTag = document.getElementsByClassName("close");
-  for (i = 0; i < spanTag.length; i++)
-    spanTag[i].onclick = function () {
-      this.parentNode.remove(this);
-    };
+    //TO DO: Add event handlers that remove the relevant list item when the delete button is pressed.
+    // When you click on the X the item next to it is removed
+    let spanTag = document.getElementsByClassName("close");
+    for (i = 0; i < spanTag.length; i++)
+      spanTag[i].onclick = function () {
+        this.parentNode.remove(this);
+      };
+
+    $("#new-item").removeClass("red-border");
+  } else {
+    $("#new-item").addClass("red-border");
+    console.log("red border");
+  }
 }
 
 //TO DO: Add event handlers that remove the relevant list item when the delete button is pressed.
